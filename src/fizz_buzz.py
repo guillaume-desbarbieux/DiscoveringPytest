3706d9d31f97eda9c19c65ea3c356eb9bbb2a0c9
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-18 16:45:27 +0200

    FizzBuzz is finish

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -2,21 +2,36 @@ def list_fizz_buzz(max):
     result = []
     for i in range(1,max + 1):
 
-        if i % 15 == 0:
-            result.append("FizzBuzz")
-            continue
-
-        if i % 3 == 0:
-            result.append("Fizz")
-            continue
-
-        if i % 5 == 0:
-            result.append("Buzz")
-            continue
-
-        result.append(i)
+        result.append(fizz_buzz(i))
 
     return result
+
+def fizz_buzz(number):
+    fizz,buzz = count_fizz_buzz(number)
+
+    if fizz == 0 and buzz == 0:
+        return number
+    else :
+
+        return "Fizz" * fizz + "Buzz" * buzz
+
+def count_fizz_buzz(number):
+    fizz = 0
+    buzz = 0
+
+    if "3" in str(number):
+        fizz += 1
+
+    if "5" in str(number):
+        buzz += 1
+
+    if number % 3 == 0:
+        fizz += 1
+
+    if number % 5 == 0:
+        buzz += 1
+    return fizz,buzz
+
 
 if __name__ == "__main__":
     print(list_fizz_buzz(100))
--- test/test_fizz_buzz.py
+++ test/test_fizz_buzz.py
@@ -3,20 +3,23 @@ from src.fizz_buzz import list_fizz_buzz
 def test_list_1_to_3():
     result = list_fizz_buzz(3)
 
-    assert result ==  [1, 2, "Fizz"]
+    assert result ==  [1, 2, "FizzFizz"]
 
 def test_list_1_to_5():
     result = list_fizz_buzz(5)
 
-    assert result ==  [1, 2, "Fizz", 4, "Buzz"]
+    assert result ==  [1, 2, "FizzFizz", 4, "BuzzBuzz"]
 
 def test_list_1_to_15():
     result = list_fizz_buzz(15)
 
-    assert result ==   [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+    assert result ==   [1, 2, 'FizzFizz', 4, 'BuzzBuzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', "Fizz", 14, 'FizzBuzzBuzz']
 
 def test_list_1_to_30():
     result = list_fizz_buzz(30)
-    assert result ==[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz']
+    assert result ==[1, 2, 'FizzFizz', 4, 'BuzzBuzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', "Fizz", 14, 'FizzBuzzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, "Fizz", 'Fizz', 'BuzzBuzz', 26, 'Fizz', 28, 29, 'FizzFizzBuzz']
 
-    
+def test_list_1to_100():
+    result = list_fizz_buzz(100)
+    assert result == [1, 2, 'FizzFizz', 4, 'BuzzBuzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 'Fizz', 14, 'FizzBuzzBuzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 'Fizz', 'Fizz', 'BuzzBuzz', 26, 'Fizz', 28, 29, 'FizzFizzBuzz', 'Fizz', 'Fizz', 'FizzFizz', 'Fizz', 'FizzBuzzBuzz', 'FizzFizz', 'Fizz', 'Fizz', 'FizzFizz', 'Buzz', 41, 'Fizz', 'Fizz', 44, 'FizzBuzzBuzz', 46, 47, 'Fizz', 49, 'BuzzBuzz', 'FizzBuzz', 'Buzz', 'FizzBuzz', 'FizzBuzz', 'BuzzBuzz', 'Buzz', 'FizzBuzz', 'Buzz', 'Buzz', 'FizzBuzz', 61, 62, 'FizzFizz', 64, 'BuzzBuzz', 'Fizz', 67, 68, 'Fizz', 'Buzz', 71, 'Fizz', 'Fizz', 74, 'FizzBuzzBuzz', 76, 77, 'Fizz', 79, 'Buzz', 'Fizz', 82, 'Fizz', 'Fizz', 'BuzzBuzz', 86, 'Fizz', 88, 89, 'FizzBuzz', 91, 92, 'FizzFizz', 94, 'BuzzBuzz', 'Fizz', 97, 98, 'Fizz', 'Buzz']
+
